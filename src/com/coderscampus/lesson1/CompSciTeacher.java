28df4aa06a4c6c3c3d54651e3bfbc47516dd8105
Dave Naugler <davenaugler@gmail.com>
2023-06-12 10:52:52 -0600

    .gitignore update

--- src/com/coderscampus/lesson1/CompSciTeacher.java
+++ src/com/coderscampus/lesson1/CompSciTeacher.java
@@ -7,6 +7,8 @@ public class CompSciTeacher extends Teacher {
 		System.out.println("Inside the CompSciTeacher constructor");
 		this.setClasses("COMP 101, COMP 201");
 		this.setLevelOfEducation("Bachelors of Education");
+		
+		// new code 
 	}
 
 }
